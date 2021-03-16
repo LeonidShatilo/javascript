8f9459686efbdfb164a9d15e660eed9eccaff21b
Leonid Shatilo <shatiloleonid@gmail.com>
2021-03-16 20:01:58 +0300

    feat: add module for utilities

---
+++ task-03-sum-of-range/src/scripts/utils.js
@@ -0,0 +1,4 @@
+export const MIN = document.getElementById('min');
+export const MAX = document.getElementById('max');
+export const SUMMING_BUTTON = document.querySelector('.summing-button');
+export const ERROR_TEXT = document.querySelector('.error');
